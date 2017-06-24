435ea66c160dbc7ce256d90fa1efe281a4992534
Susan Heiniger <susan.heiniger@gmail.com>
2017-06-24 14:59:49 -0500

    building out file structure

---
+++ app/components/utils/helpers.js
@@ -0,0 +1,45 @@
+b9f91d369ff59547cd47b931d8cbc56b:0:74623931
+
+// Include the axios package for performing HTTP requests (promise based alternative to request)
+var axios = require("axios");
+
+// Geocoder API
+var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+// Helper functions for making API Calls
+var helper = {
+
+  // This function serves our purpose of running the query to geolocate.
+  runQuery: function(articleScrape) {
+
+    console.log(articleScrape);
+
+    // Figure out the geolocation
+    var queryURL = "http://https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+    return axios.get(queryURL).then(function(response) {
+      // If get get a result, return that result's formatted address property
+
+//Rework the results index
+      if (response.data.results[0]) {
+        return response.data.results[0].formatted;
+      }
+      // If we don't get any results, return an empty string
+      return "";
+    });
+  },
+
+  // This function hits our own server to retrieve the record of query results
+  getHistory: function() {
+    return axios.get("/api");
+  },
+
+  // This function posts new searches to our database.
+  postHistory: function(articleScrape) {
+    return axios.post("/api", { location: location });
+
+//"location: location" ????????
+  }
+};
+
+// We export the API helper
+module.exports = helper;
---
+++ server.js
@@ -0,0 +1,135 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+//Scraping Tools
+
+//TODO add dependencies
+var request = require("request")
+var cheerio = require("cheerio")
+
+// Require History Schema
+var Articles = require("./models/Article");
+
+//Mongoose Promise
+mongoose.Promise = Promise;
+
+// Create Instance of Express
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));
+
+// -------------------------------------------------
+
+// MongoDB Configuration configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://heroku_m44l7x4r:skc3ncnd94jjqcu5osirv3i26t@ds137882.mlab.com:37882/heroku_m44l7x4r");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+
+// Main "/" Route. This will redirect the user to our rendered React application
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// This is the route we will send GET requests to retrieve our most recent search data.
+// We will call this route the moment our page gets rendered
+app.get("/api", function(req, res) {
+
+  // We will find all the records, sort it in descending order, then limit the records to 5
+  History.find({}).sort([
+    ["date", "descending"]
+  ]).limit(5).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+// A GET request to scrape the NYT website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.nyt.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+
+// This is the route we will send POST requests to save each search.
+app.post("/api", function(req, res) {
+  console.log("BODY: " + req.body.location);
+
+  // Here we'll save the location based on the JSON input.
+  // We'll use Date.now() to always get the current date time
+  History.create({
+    location: req.body.location,
+    date: Date.now()
+  }, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.send("Saved Search");
+    }
+  });
+});
+
+// -------------------------------------------------
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
